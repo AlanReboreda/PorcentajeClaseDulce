9d48210214f63c1bad70e2b7a8a36f8f41ca76a7
AlanReboreda <92063942+AlanReboreda@users.noreply.github.com>
2021-10-07 19:10:54 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+let btnEnv = document.getElementById("btnEnviar");
+let dato = document.getElementById("dato");
+let dato2 = document.getElementById("dato2");
+
+btnEnv.addEventListener("click", () => {
+  let numeroA: number = Number(dato.value);
+  let numeroB: number = Number(dato2.value);
+  let resultado: number = (numeroA / "100") * numeroB;
+  console.log("el descuento es:");
+  console.log(resultado);
+});
