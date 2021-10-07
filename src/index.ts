346cd9eca49484e5570abb51665b5d6c28a57236
AlanReboreda <92063942+AlanReboreda@users.noreply.github.com>
2021-10-07 19:20:50 -0300

    CambioEnElCodigo cambie el código del porcentaje

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,8 @@ let dato2 = document.getElementById("dato2");
 btnEnv.addEventListener("click", () => {
   let numeroA: number = Number(dato.value);
   let numeroB: number = Number(dato2.value);
-  let resultado: number = (numeroA / "100") * numeroB;
+  let porcentaje: number = numeroA / "100";
+  let resultado: number = porcentaje * numeroB;
   console.log("el descuento es:");
   console.log(resultado);
 });
